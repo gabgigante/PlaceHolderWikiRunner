2775be6ae5f82bfe667762db633dea7b08e5f47c
gabgigante <127763621+gabgigante@users.noreply.github.com>
2026-05-12 17:07:27 +0200

    first implementation of script.js and api in main.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-const button = document.getElementById("open");
+// const button = document.getElementById("open");
 const result = document.getElementById("display-result");
-const counterDisplay = document.getElementById("click-counter"); // aggiungi questo elemento nel tuo HTML
+const counterDisplay = document.getElementById("jumps");
+const seconds = document.getElementById("seconds");
+const minutes = document.getElementById("minutes");
 
+let countSec = 0;
+let countMins = 0;
 let currentTitle = "";
 let clickCount = 0;
 
@@ -140,15 +144,25 @@ async function getWikiAPI() {
   }
 }
 
-if (button && result) {
+/*if (button && result) {
   button.addEventListener("click", () => {
     console.log("Nuovo articolo casuale...");
     getWikiAPI();
   });
 } else {
   console.error("Elementi HTML non trovati!");
-}
+}*/
 
 document.addEventListener("DOMContentLoaded", () => {
   getWikiAPI();
 });
+
+setInterval(() => {
+  countSec++;
+  if (countSec > 59) {
+    countSec = 0;
+    countMins++;
+  }
+  seconds.textContent = countSec < 10 ? "0" + countSec : countSec;
+  minutes.textContent = countMins < 10 ? "0" + countMins : countMins;
+}, 1000);
