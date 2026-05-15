2feacdcefb64b5a4b2ade97f6bfed4aa7a9a58d4
saccoviola <deniserizzi94@gmail.com>
2026-05-15 13:14:55 +0200

    fixed enter button and mobile layout with focus on accessibility

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -29,9 +29,12 @@ const bootLines = [
 
 ]
 
-window.addEventListener("keydown", (e)=>{
-    if(e.key === "enter"){
-         window.location.href = "../../index.html";
+document.addEventListener("keydown", (e)=>{
+    if(e.key === "Enter"){
+
+        window.location.href = "../../wikirun.html";
+
+        //  window.location.href = "/PlaceHolderWikiRunner/wikirun.html";
 
     }
 })
@@ -39,24 +42,24 @@ window.addEventListener("keydown", (e)=>{
 startBootLines();
 
 function startBootLines() {
-  let totalDelay = 0;
+    let totalDelay = 0;
+    let bootContainer = document.createElement("section");
+    bootContainer.classList.add("boot-container")
 
   for (let i = 0; i < bootLines.length; i++) {
     const line = bootLines[i];
     totalDelay += line.delay;
 
     setTimeout(() => {
-      let newLine = document.createElement("p");
-      newLine.textContent = line.paragraph;
-
-      // if (line.status) {
-      //     newLine.classList.add(line.status);
-      // }
-
-      loadingContainer.append(newLine);
+    let newLine = document.createElement("p");
+    newLine.textContent = line.paragraph;
+    loadingContainer.append(bootContainer);
+    bootContainer.append(newLine);
     }, totalDelay);
   }
   setTimeout(() => {
-    window.location.href = "/PlaceHolderWikiRunner/wikirun.html";
+
+    window.location.href = "../../wikirun.html";
+    // window.location.href = "../PlaceHolderWikiRunner/wikirun.html";
   }, totalDelay + 500);
 }
